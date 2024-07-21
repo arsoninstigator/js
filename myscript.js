f94406b36d345cc6c29208cae1d6124d0a922dd8
anne <anoushkabh@outlook.com>
2024-07-21 11:47:04 +0530

    Update myscript.js
    copy pasting from sessions

--- myscript.js
+++ myscript.js
@@ -1,3 +1,45 @@
 function myFunction() {
   document.getElementById("demo").innerHTML = "Paragraph changed.";
 }
+
+let a, b, c;
+a = 2;
+b = 3;
+c = a + b;
+console.log("The value of c is " + c + ".");
+
+function myFunction() {
+    console.log("Hello");
+    console.log("How are you?");
+}
+
+myFunction()
+
+let num1 = 50
+let num2 = 50.05
+
+let str1 = "Geek"
+let str2 = 'Geeks'
+
+console.log(num1)
+console.log(num2)
+console.log(str1)
+console.log(str2)
+
+
+// Global variable declaration
+let Name = "Apple";
+// Function definition
+function MyFunction() {
+// Local variable declaration
+    let num = 45;
+    // Display the value of Global variable
+    console.log(Name);
+    // Display the value of local variable
+    console.log(num);
+}
+
+// Function call
+MyFunction();
+
+
