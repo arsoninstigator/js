2cde823a097bca2960ee288d23d3678e14cbbc5b
anne <anoushkabh@outlook.com>
2024-08-17 22:07:08 +0530

    create script.js

---
+++ 2048/script.js
@@ -0,0 +1,29 @@
+const gridItems = [ 
+    ...document.querySelectorAll(".grid-item"), 
+]; 
+const score_val = document.querySelector(".score-value"); 
+const result = document.querySelector(".result"); 
+let score = 0; 
+let moves = 0; 
+let moveFactor = 4; 
+let options = [ 
+    2, 4, 8, 2, 4, 8, 2, 2, 4, 4, 2, 8, 2, 2, 4, 4, 2, 
+]; 
+let matrix = []; 
+let prevMatrix; 
+
+// initial game grid 
+let row = []; 
+for (let index = 1; index < gridItems.length + 1; index++) { 
+    if (index % 4 === 0) { 
+        let item = gridItems[index - 1]; 
+        item.firstElementChild.innerText = ""; 
+        row.push(item); 
+        matrix.push(row); 
+        row = []; 
+    } else { 
+        let item = gridItems[index - 1]; 
+        item.firstElementChild.innerText = ""; 
+        row.push(item); 
+    } 
+} 
